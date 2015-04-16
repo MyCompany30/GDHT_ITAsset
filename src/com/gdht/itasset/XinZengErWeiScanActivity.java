9ad99e931ace0de09d03fdb6085d2b08209a2605
MyCompany30 <maforever@126.com>
2015-04-16 17:04:40 +0800

    113333
    222

--- src/com/gdht/itasset/XinZengErWeiScanActivity.java
+++ src/com/gdht/itasset/XinZengErWeiScanActivity.java
@@ -2,17 +2,13 @@ package com.gdht.itasset;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.gdht.itasset.adapter.YingPanXinZenItemAdapter;
 import com.gdht.itasset.pojo.YingPanXinZengItem;
 import com.gdht.itasset.utils.GlobalParams;
@@ -33,6 +29,8 @@ public class XinZengErWeiScanActivity extends Activity {
 		// pd.setMessage("数据保存中...");
 		findViews();
 	}
+	
+	
 
 	private void findViews() {
 		listView = (ListView) this.findViewById(R.id.listView);
